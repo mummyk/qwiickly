23a67859b117a7f019001ac95efa6330c568b3f4
Chibuzo <chibuzoi410@yahoo.com>
2022-08-15 19:04:09 +0000

    iOS

--- source_code/ios/Flutter/flutter_export_environment.sh
+++ source_code/ios/Flutter/flutter_export_environment.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\products\qwiickly\source_code"
+export "FLUTTER_ROOT=/Users/builder/programs/flutter_3_0_5"
+export "FLUTTER_APPLICATION_PATH=/Users/builder/clone/source_code"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=3.0.0"
 export "FLUTTER_BUILD_NUMBER=16"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=false"
-export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=.dart_tool/package_config.json"
+export "TREE_SHAKE_ICONS=true"
+export "PACKAGE_CONFIG=/Users/builder/clone/source_code/.dart_tool/package_config.json"
